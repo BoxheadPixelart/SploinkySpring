11b7242301c2276f0b3651132849a9ebbcb7b07c
Storm Hughes <storm.hughes8@gmail.com>
2022-09-25 19:48:21 -0500

    SploinkyDemo

---
+++ assets/SploinkyTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Storm.SploinkySpring; 
+public class SploinkyTest : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
